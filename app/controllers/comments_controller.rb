4657aeb1252a311c08c7cd5a7b631a3198f512d1
Teodor Mavrodiev <teodor.mavrodiev@gmail.com>
2017-06-07 21:38:42 +0300

    finished authentication and authorization on working functionality thus far

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,23 +6,30 @@ class CommentsController < ApplicationController
     comment.datetime = DateTime.now
     comment.information = params["Your Comment"]
     comment.user = current_user
+
+    authorize comment
     comment.save!
     redirect_to :back
   end
 
   def edit
     @comment = Comment.find(params[:id])
+    authorize @comment
   end
 
   def update
     comment = Comment.find(params[:id])
     comment.information = params["Your Comment"]
+
+    authorize comment
     comment.save!
     redirect_to reservations_path
   end
 
   def destroy
     comment = Comment.find(params[:id])
+
+    authorize comment
     comment.destroy!
     redirect_to reservations_path
   end
